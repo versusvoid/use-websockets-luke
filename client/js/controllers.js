32d8abb69a2e239bb7318a287350119f2ce72157
versus void <versusvoid@gmail.com>
2014-04-02 15:03:18 +0000

    Reconnect

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,27 +4,44 @@
 
 var ncumcControllers = angular.module('ncumcControllers', []);
 
-ncumcControllers.controller('Welcome', ['$scope', '$log', '$location', 'connection',
-  function($scope, $log, $location, connection) {
+ncumcControllers.controller('Welcome', ['$scope', '$log', '$location', '$timeout', 'connection',
+  function($scope, $log, $location, $timeout, connection) {
+    $scope.connecting = false;
+    $scope.error = null;
+    $scope.connectTimeout = null;
+
     $scope.connect = function() {
-      $scope.connecting = true;
       $scope.error = null;
+      $scope.connecting = true;
+      $scope.connectTimeout && $timeout.cancel($scope.connectTimeout);
       connection.connect($scope.address)
         .then(function(table) {
            $location.url('/table'); 
         }, function(event) {
-            $scope.error = {};
             if (!event.reason || event.reason === "") {
-                $scope.error.reason = "Whatever!";
+                $scope.error = "Can't connect";
             } else {
-                $scope.error.reason = event.reason;
+                $scope.error = event.reason;
             }
             $scope.connecting = false;
+            $scope.setConnectTimeout();
         });
     };
+
+    $scope.setConnectTimeout = function() {
+      $log.debug('setting connect timeout');
+      $scope.connectTimeout = $timeout($scope.connect, 10000);
+    };
+
+    $scope.addressChanged = function() {
+      $log.debug('Address changed');
+      $scope.error = null;
+      $scope.connectTimeout && $timeout.cancel($scope.connectTimeout);
+    };
+
     if (connection.address !== null) {
       $scope.address = connection.address;
-      $scope.connect();
+      $scope.setConnectTimeout();
     }
   }]);
 
